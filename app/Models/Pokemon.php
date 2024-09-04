0103ea238d99eeece32f0b8e57bd845006833bcf
ZyFou <zyfodexe@gmail.com>
2024-09-04 13:54:21 +0200

    Build : Edited model files
    Affected :
    pokemon.php
    pokemonTranslation.php

--- app/Models/Pokemon.php
+++ app/Models/Pokemon.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
+use Astrotomic\Translatable\Translatable;
 
-class Pokemon extends Model
+class Pokemon extends Model implements TranslatableContract
 {
-    use HasFactory;
+    use HasFactory, Translatable;
+
+    // Liste des attributs traduits
+    public $translatedAttributes = ['name', 'category'];
+
+    // Liste des attributs autorisés pour la création et la mise à jour des données de manière massive
+    protected $fillable = ['has_gender_differences', 'is_baby', 'is_legendary', 'is_mythical'];
+
+    // Définition des types de données des attributs
+    protected $casts = [
+        'has_gender_differences' => 'boolean',
+        'is_baby' => 'boolean',
+        'is_legendary' => 'boolean',
+        'is_mythical' => 'boolean',
+    ];
 }
