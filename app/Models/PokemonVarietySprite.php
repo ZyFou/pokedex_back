30dcc56a789df47138df11008618f59adb5dc836
ZyFou <zyfodexe@gmail.com>
2024-09-04 14:17:37 +0200

    Build : Added PokemonVarietySprite table

---
+++ app/Models/PokemonVarietySprite.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PokemonVarietySprite extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'artwork_url',
+        'artwork_shiny_url',
+        'front_url',
+        'front_female_url',
+        'front_shiny_url',
+        'front_shiny_female_url',
+        'back_url',
+        'back_female_url',
+        'back_shiny_url',
+        'back_shiny_female_url'
+    ];
+}
